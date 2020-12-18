b7702d0037f81f639728aa2bae9bb20044a1c043
Saddam <khansaddamhussain22016@gmail.com>
2020-12-18 20:36:43 +0530

    home page added

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
+with open('config.json', 'r') as c:
+    params = json.load(c)["parameters"]
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Hello World"
+@app.route('/home')
+def home():
+    return render_template("home.html", title="home")
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template("portfolio.html", title="portfolio")
+
+@app.route('/services')
+def services():
+    return render_template("services.html", title="services")
+
+@app.route('/quote')
+def quote():
+    return render_template("quote.html", title="quote")
 
 
 if __name__ == "__main__":
